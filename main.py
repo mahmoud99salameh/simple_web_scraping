ff141323fc3bb111e47c71adea9113e1a9accc2f
mahmoud99salameh <mahmoud99salameh@gmail.com>
2023-11-14 15:31:38 +0200

    31th commit

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ def SimpleWebScraper(url):
     response=requests.get(url)
     #checking if its successfully connected
     if response.status_code==200:
-        
+        #parsing the html code 
+        soup=BeautifulSoup(response.text,'html.parser')
+        #extract the quotes 
+        for i , quote in enumerate(quote,start=1):
+            print(f"Quote {i} : {quote.text.strip()}")
+    else:
+        print("Error")
